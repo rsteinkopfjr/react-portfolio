e9d615bf51b3f319ab1de313a5b3e590cdf80f7c
rsteinkopfjr <rob.steinkopf@gmail.com>
2019-06-07 13:49:40 -0400

    Add landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./images/RS_Logo.png";
+import "./App.css";
 
 class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      theme: "",
+      lightBtnHover: false,
+      darkBtnHover: false
+    };
+
+    this.themeLight = this.themeLight.bind(this);
+    this.hoverLight = this.hoverLight.bind(this);
+    this.themeDark = this.themeDark.bind(this);
+    this.hoverDark = this.hoverDark.bind(this);
+  }
+  themeLight() {
+    this.setState({
+      theme: "light"
+    });
+  }
+  hoverLight() {
+    this.setState({
+      lightBtnHover: true
+    });
+  }
+  themeDark() {
+    this.setState({
+      theme: "dark"
+    });
+  }
+  hoverDark() {
+    this.setState({
+      darkBtnHover: true
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div
+          className={
+            this.state.lightBtnHover
+              ? "lightHovered App-header"
+              : "App-header" && this.state.darkBtnHover
+              ? "darkHovered App-header"
+              : "App-header"
+          }
+        >
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="jumbotron jumbotron-fluid">
+            <div className="container">
+              <h1 className="display-4">Choose a Theme</h1>
+              <div className="row">
+                <div className="col-6">
+                  <button
+                    onClick={this.themeLight}
+                    onMouseEnter={this.hoverLight}
+                    onMouseLeave={() => this.setState({ lightBtnHover: false })}
+                    type="button"
+                    className="btn btn-outline-light btn-lg"
+                    id="lightSide"
+                  >
+                    Light
+                  </button>
+                </div>
+                <div className="col-6">
+                  <button
+                    onClick={this.themeDark}
+                    onMouseEnter={this.hoverDark}
+                    onMouseLeave={() => this.setState({ darkBtnHover: false })}
+                    type="button"
+                    className="btn btn-outline-dark btn-lg"
+                    id="darkSide"
+                  >
+                    Dark
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,6 @@ import "./index.css";
 import App from "./App";
 import serviceWorker from "./serviceWorker";
 
+
 ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker();
